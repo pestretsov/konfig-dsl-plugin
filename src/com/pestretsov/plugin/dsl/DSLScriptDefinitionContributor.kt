59b75cfe2f6e7edb234cf1d65d350963a6002c48
pestretsov <pestretsov@me.com>
2018-04-20 01:55:06 +0300

    fixed DSLScriptDefinitionContributor and removed unused files

--- src/com/pestretsov/plugin/dsl/DSLScriptDefinitionContributor.kt
+++ src/com/pestretsov/plugin/dsl/DSLScriptDefinitionContributor.kt
@@ -1,24 +1,25 @@
 package com.pestretsov.plugin.dsl
 
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.diagnostic.Logger
 import org.jetbrains.kotlin.idea.core.script.ScriptDefinitionContributor
 import org.jetbrains.kotlin.idea.core.script.loadDefinitionsFromTemplates
 import org.jetbrains.kotlin.script.KotlinScriptDefinition
 import java.io.File
 
+const val PATH_TO_DSL_JAR = "/Users/artemypestretsov/IdeaProjects/konfing-dsl-test/helper/sample-dsl.jar"
+const val DSL_CONFIG_CLASS = "com.example.AgentConfig"
 
-class DSLScriptDefinitionContributor(val project: Project) : ScriptDefinitionContributor {
+private val LOG = Logger.getInstance("ScriptTemplatesProviders")
+
+class DSLScriptDefinitionContributor : ScriptDefinitionContributor {
     override val id = "DSLScriptDefinitionContributor"
 
     override fun getDefinitions(): List<KotlinScriptDefinition> {
-
-        println("loading defs")
+        LOG.info("Loading Konfig DSL definitions")
 
         return loadDefinitionsFromTemplates(
                 templateClassNames = listOf(DSL_CONFIG_CLASS),
-                templateClasspath = listOf(File(PATH_TO_DSL_JAR)),
-                environment = mapOf(
-                        "projectRoot" to (project.basePath ?: project.baseDir.canonicalPath)?.let(::File))
+                templateClasspath = listOf(File(PATH_TO_DSL_JAR))
         )
     }
 }
